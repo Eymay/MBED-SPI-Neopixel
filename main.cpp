d7aa3d22c4a4fcd5b19344a63216e771aed201c1
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2022-12-11 21:18:32 +0300

    Led lighted

--- main.cpp
+++ main.cpp
@@ -1,32 +1,9 @@
 #include "mbed.h"
+#include <cstdint>
 
 SPI spi(D11, D12, D13); // mosi, miso, sclk
 DigitalOut cs(D10);
-int main()
-{
-    spi.set_dma_usage(DMA_USAGE_ALWAYS);
-    //spi.format(8, 3);
-    spi.format(8, 1);
-    spi.frequency(10000000);
 
-    //spi.write(0xAA);
-    //spi.write(0xAA);
-
-    while (true) {
-        //wait_us(100000);
-        cs = 0;
-        spi.write(0xFC);
-        //spi.write(0xE0);
-        cs = 1;
-        //cs = 0;
-        //spi.write(0xFC);
-        //spi.write(0xFC);
-        //cs = 1;
-        //wait_us(2);
-        //spi.write(0xAE);
-    }
-     
-}
 
 void write_one(){
     cs = 0;
@@ -40,5 +17,50 @@ void write_zero(){
     spi.write(0xE0);
     cs = 1;
     return;
+}
+
+bool last_bit(uint32_t data){
+    return (data & 0x00000001); 
+}
+
+
+int main()
+{
+    spi.set_dma_usage(DMA_USAGE_ALWAYS);
+    //spi.format(8, 3);
+    spi.format(8, 1);
+    spi.frequency(10000000);
+
+    //spi.write(0xAA);
+    //spi.write(0xAA);
+
+
+
+    while (true) {
+    uint32_t data = 0x00FF0000;
+        //wait_us(100000);
+            for (int i = 0; i < 24; i++) {
+
+            if(last_bit(data)){
+                write_one();
+            }else {
+                write_zero();
+            }
+            data = data >> 1;
+            }
+            wait_us(300);
+        //cs = 0;
+        //spi.write(0xFC);
+        //spi.write(0xE0);
+        //cs = 1;
+        //cs = 0;
+        //spi.write(0xFC);
+        //spi.write(0xFC);
+        //cs = 1;
+        //wait_us(2);
+        //spi.write(0xAE);
+
+    }
+     
 }
 
