b5026151ab22d04b466b78f6ee7ab4f4a5599bb6
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2022-12-11 20:39:49 +0300

    bit pwm pulses of 0.375us and 0.75us are created

--- main.cpp
+++ main.cpp
@@ -1,15 +1,44 @@
 #include "mbed.h"
 
 SPI spi(D11, D12, D13); // mosi, miso, sclk
-// main() runs in its own thread in the OS
+DigitalOut cs(D10);
 int main()
 {
-    spi.format(8, 3);
-    spi.frequency(1000000);
+    spi.set_dma_usage(DMA_USAGE_ALWAYS);
+    //spi.format(8, 3);
+    spi.format(8, 1);
+    spi.frequency(10000000);
 
-    spi.write(0x8F);
+    //spi.write(0xAA);
+    //spi.write(0xAA);
 
-    //while (true) {}
+    while (true) {
+        //wait_us(100000);
+        cs = 0;
+        spi.write(0xFC);
+        //spi.write(0xE0);
+        cs = 1;
+        //cs = 0;
+        //spi.write(0xFC);
+        //spi.write(0xFC);
+        //cs = 1;
+        //wait_us(2);
+        //spi.write(0xAE);
+    }
      
+}
+
+void write_one(){
+    cs = 0;
+    spi.write(0xFC);
+    cs = 1;
+    return;
+}
+
+void write_zero(){
+    cs = 0;
+    spi.write(0xE0);
+    cs = 1;
+    return;
 }
 
