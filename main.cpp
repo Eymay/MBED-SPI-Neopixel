480d461c3f16aa6d8e15ca9a8a0416cc106191f8
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2022-12-11 17:26:52 +0300

    spi first try

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include "mbed.h"
 
+SPI spi(D11, D12, D13); // mosi, miso, sclk
 // main() runs in its own thread in the OS
 int main()
 {
-    while (true) {
+    spi.format(8, 3);
+    spi.frequency(1000000);
 
-    }
+    spi.write(0x8F);
+
+    //while (true) {}
+     
 }
 
