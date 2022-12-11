9b157a6335364633ebea3731aceda2157645dfed
Eymen Ünay <20779470+Eymay@users.noreply.github.com>
2022-12-11 21:30:49 +0300

    notes

---
+++ neopixel.h
@@ -0,0 +1,4 @@
+void initNeo(void **handle); //Initializes SPI interface and returns a handle to an opaque structure that you 
+can declare in your library. 
+setNeoRGB(void *handle, uint8_t r, uint8_t g, uint8_t b); //Turns on the neopixel with the provided r, g, b 
+(red, green, blue) color values. It takes the handle obtained by initNeo function.
